2df3756e4fcaf958d9cc371c2b6b813b489d6bac
Guido <ppapaguido@gmail.com>
2022-03-11 10:39:19 +0100

    aggiunta immagini nel seed

--- database/seeds/TypologiesTableSeeder.php
+++ database/seeds/TypologiesTableSeeder.php
@@ -14,14 +14,54 @@ class TypologiesTableSeeder extends Seeder
     public function run()
     {
         $typologies = [
-            'pizza', 'italiano', 'sushi', 'dessert', 'poke', 'americano', 'gelato', 'kebab', 'sandwich','hamburger'
+            [
+                'name' => 'pizza',
+                'image' => '1bHQflpj4uhmW2ntZKmDd_hK_vcNg-y_w'
+            ],
+            [
+                'name' => 'italiano',
+                'image' => '1QA4EgXEzAi35UGk8mxporB7hY-P0nxep'
+            ],
+            [
+                'name' => 'dessert',
+                'image' => '1aQ2i91S1PiAKVpCh-EDsE6N3ByHQo0a5'
+            ],
+            [
+                'name' => 'sushi',
+                'image' => '18tIe9EBHznAWYVa9p710GL-u3yCkJuyE'
+            ],
+            [
+                'name' => 'poke', 
+                'image' => '1pImPM2fk8Q9eK5I4p1defUn9hbquNWus'
+            ],
+            [
+                'name' => 'americano', 
+                'image' => '1ceo7PvgNrDwpZh6XePBC46ekhu-91ffu'
+            ],
+            [
+                'name' => 'gelato', 
+                'image' => '15DNT3aljcHnd6wlL_-yQOFVbgHpDBkhx'
+            ],
+            [
+                'name' => 'kebab', 
+                'image' => '1RRe_f5jtf4ElASDMnxs5-7ZOzbJz5KiO'
+            ],
+            [
+                'name' => 'sandwich', 
+                'image' => '1HN_PrTjXZrQyksBeDz-n3FrS8G4WIiCJ'
+            ],
+            [
+                'name' => 'hamburger', 
+                'image' => '1ceo7PvgNrDwpZh6XePBC46ekhu-91ffu'
+            ],
+
         ];
 
         foreach ($typologies as $typology) {
             $newTypology = new Typology();
-            $newTypology->type = $typology;
+            $newTypology->type = $typology['name'];
             $newTypology->slug = Str::of($newTypology->type)->slug('-');
-            // $newTypology->image = ####; 
+            $newTypology->image = $typology['image']; 
             $newTypology->save();
         }
     }
