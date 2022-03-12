ff7489af89693927c7677a0dc3d3275999792e11
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-03-12 11:07:08 +0100

    bozza index dish and show dish

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    @foreach ($dishes as $dish)
+        <div class="row my-2">
+            <div class="col">{{$dish->name}}</div>
+            <div class="col">{{$dish->price}}</div>
+            @if($dish->visible)
+                <div class="col btn btn-success">Visibile</div>
+            @else
+                <div class=" col btn btn-danger">Visibile</div>
+            @endif
+
+            {{-- Sezione Bottoni Piatto --}}
+            <div class="col">
+                <a href="{{route("dishes.show", $dish->id)}}"><button type="button" class="btn">Visualizza</button></a>
+            </div>
+            <div class="col">
+                <a href="{{route("dishes.edit", $dish->id)}}"><button type="button" class="btn">Modifica</button></a>
+            </div>
+            <div class="col">
+                <form action="{{route("dishes.destroy", $dish->id)}}" method="POST">
+                    @csrf
+                    @method("DELETE")
+                    <button type="submit" class="btn">Elimina</button>
+                </form>
+            </div>
+        </div>
+    @endforeach
+</div>
 
 @endsection
--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -2,4 +2,38 @@
 
 @section('content')
 
+<div class="container">
+    {{-- Sezione Immagine Piatto --}}
+    @if ($dish->image)
+        <img src="{{asset("storage/{$dish->image}")}}" alt="{{$dish->name}}">
+    @endif
+
+    {{-- Sezione Ingredienti --}}
+    <div>
+        <h6>Ingredineti</h6>
+        <div>{{$dish->ingredients}}</div>
+    </div>
+
+    {{-- Sezione Descrizione --}}
+    <div>
+        <h6>Descrizione</h6>
+        <div>{{$dish->description}}</div>
+    </div>
+
+    {{-- Sezione Prezzo --}}
+    <div>
+        <h6>Prezzo</h6>
+        <div>{{$dish->price}}</div>
+    </div>
+
+    {{-- Sezione Visibile --}}
+    <div>
+        @if($dish->visible)
+            <div class="btn btn-success">Visibile</div>
+        @else
+            <div class="btn btn-danger">Visibile</div>
+        @endif
+    </div>
+</div>
+
 @endsection
