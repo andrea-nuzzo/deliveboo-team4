234de0c9ea61ea098af46000e6143defb21a9d7c
Guido <ppapaguido@gmail.com>
2022-03-12 16:59:07 +0100

    fine seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,22 +16,105 @@ class UsersTableSeeder extends Seeder
     {
         $users = [
             [
-                'name' => 'Anais Restaurant',
-                'email' => 'bistrotrestaurant@gmail.com',
+                'name' => 'Porca Vacca',
+                'email' => 'porcavacca@gmail.com',
                 'password' => bcrypt('prova123'),
-                'address' => 'via 1',
-                'p_iva' => '22334455668',
-                'image' => 'uploads/jury.jpeg',
-                'phone' => '1234567890',
+                'address' => 'Via Marco Polo - c/o ipermercato Leone Agostini, Viareggio, 55049',
+                'p_iva' => '22334455669',
+                'image' => 'uploads/porca-vacca.jpeg',
+                'phone' => '0564 28699',
+                'typologies' => 
+                    [
+                        '6',
+                        '10',
+                    ]
+            ],
+            [
+                'name' => 'Mondo Kebab',
+                'email' => 'mondokebab@gmail.com',
+                'password' => bcrypt('prova123'),
+                'address' => 'Via Coppino 333, Viareggio, 55049',
+                'p_iva' => '21334455669',
+                'image' => 'uploads/mondo-kebab.jpeg',
+                'phone' => '344 2677343',
                 'typologies' => 
                     [
                         '1',
+                        '8',
+                    ]
+            ],
+            [
+                'name' => 'Sakura Fusion',
+                'email' => 'sakurafusion@gmail.com',
+                'password' => bcrypt('prova123'),
+                'address' => 'Viale Europa 29, Viareggio, 55049',
+                'p_iva' => '21234455669',
+                'image' => 'uploads/sakura-fusion.jpeg',
+                'phone' => '0584 1640107',
+                'typologies' => 
+                    [
+                        '4',
+                    ]
+            ],
+            [
+                'name' => 'Dolce Mazzini',
+                'email' => 'dolcemazzini@gmail.com',
+                'password' => bcrypt('prova123'),
+                'address' => 'Via Giusseppe Mazzini, 184, Viareggio, 55049',
+                'p_iva' => '21231455669',
+                'image' => 'uploads/dolce-mazzini.jpeg',
+                'phone' => '350 9204721',
+                'typologies' => 
+                    [
                         '3',
+                        '1'
+                    ]
+            ],
+            [
+                'name' => 'Mirice Poke - Pokeria',
+                'email' => 'miricepoke@gmail.com',
+                'password' => bcrypt('prova123'),
+                'address' => "25 Piazza Massimo D'Azeglio, Viareggio, 55049",
+                'p_iva' => '21231455969',
+                'image' => 'uploads/mirice-poke.jpeg',
+                'phone' => '0584 344567',
+                'typologies' => 
+                    [
+                        '5',
+                    ]
+            ],
+            [
+                'name' => 'Dolcemaro',
+                'email' => 'dolcemaro@gmail.com',
+                'password' => bcrypt('prova123'),
+                'address' => "Via Giuseppe Mazzini, 110, Viareggio, 55049",
+                'p_iva' => '91231455969',
+                'image' => 'uploads/dolcemaro.jpeg',
+                'phone' => '0584 1842096',
+                'typologies' => 
+                    [
+                        '2',
                         '7'
                     ]
             ],
+            [
+                'name' => 'La Maison Del Tramezzino',
+                'email' => 'lamaisondeltramezzino@gmail.com',
+                'password' => bcrypt('prova123'),
+                'address' => "Via San Martino 174, Viareggio, 55049",
+                'p_iva' => '91931455969',
+                'image' => 'uploads/tramezzino.jpeg',
+                'phone' => '348 5841855',
+                'typologies' => 
+                    [
+                        '9',
+                        '2'
+                    ]
+            ],
+
         ];
 
+        // americano , hamburger , kebab, pizza, sushi, dessert, poke, gelato
 
         foreach ($users as $user) {
             $newUser = new User();
@@ -46,7 +129,7 @@ class UsersTableSeeder extends Seeder
 
             $newUser->save();
             
-            $newUser->typologies()->attach($user['typologies']);
+            $newUser->typologies()->sync($user['typologies']);
         }
     }
 }
