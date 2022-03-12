e1d0c376b7a98f48ccc13e139a6c4d7d3f4f3ce7
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-03-12 11:17:11 +0100

    creato if user_id per visuallizzazione piatti

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -3,30 +3,32 @@
 @section('content')
 <div class="container">
     @foreach ($dishes as $dish)
-        <div class="row my-2">
-            <div class="col">{{$dish->name}}</div>
-            <div class="col">{{$dish->price}}</div>
-            @if($dish->visible)
-                <div class="col btn btn-success">Visibile</div>
-            @else
-                <div class=" col btn btn-danger">Visibile</div>
-            @endif
+        @if($dish->user_id == Auth::id())
+            <div class="row my-2">
+                <div class="col">{{$dish->name}}</div>
+                <div class="col">{{$dish->price}}</div>
+                @if($dish->visible)
+                    <div class="col btn btn-success">Visibile</div>
+                @else
+                    <div class=" col btn btn-danger">Visibile</div>
+                @endif
 
-            {{-- Sezione Bottoni Piatto --}}
-            <div class="col">
-                <a href="{{route("dishes.show", $dish->id)}}"><button type="button" class="btn">Visualizza</button></a>
+                {{-- Sezione Bottoni Piatto --}}
+                <div class="col">
+                    <a href="{{route("dishes.show", $dish->id)}}"><button type="button" class="btn">Visualizza</button></a>
+                </div>
+                <div class="col">
+                    <a href="{{route("dishes.edit", $dish->id)}}"><button type="button" class="btn">Modifica</button></a>
+                </div>
+                <div class="col">
+                    <form action="{{route("dishes.destroy", $dish->id)}}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn">Elimina</button>
+                    </form>
+                </div>
             </div>
-            <div class="col">
-                <a href="{{route("dishes.edit", $dish->id)}}"><button type="button" class="btn">Modifica</button></a>
-            </div>
-            <div class="col">
-                <form action="{{route("dishes.destroy", $dish->id)}}" method="POST">
-                    @csrf
-                    @method("DELETE")
-                    <button type="submit" class="btn">Elimina</button>
-                </form>
-            </div>
-        </div>
+        @endif
     @endforeach
 </div>
 
