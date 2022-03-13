54d5308a7d96a61973d380f552ae37110758c364
Guido <ppapaguido@gmail.com>
2022-03-13 16:25:07 +0100

    register colors

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header card--header">{{ __('Register') }}</div>
 
 
 
@@ -153,7 +153,7 @@
                         {{-- Button Register --}}
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn--green">
                                     {{ __('Register') }}
                                 </button>
                             </div>
