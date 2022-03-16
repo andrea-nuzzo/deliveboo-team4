4c98470d055c9f9f69814c72c37c315d037f960f
Guido <ppapaguido@gmail.com>
2022-03-16 11:09:07 +0100

    media query

--- resources/js/guest/sections/TypologyList.vue
+++ resources/js/guest/sections/TypologyList.vue
@@ -1,6 +1,6 @@
 <template>
       <div class="container-fluid mt-3">
-        <vue-horizontal>
+        <vue-horizontal class="horizontal">
             <div v-for="typology in listTypo" :key="typology.id" class="card--typo">
                 <div>{{typology.type}}</div>
                 <div class="containerImage">
@@ -21,14 +21,50 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-
-
 .card--typo{
-    width: calc(100% / 6 - 10px);
     margin: 0 5px;
+    width: calc(100%  / 3 - 10px);
     
     img {
         width:100%;
     }
 }
+@media (min-width: 640px) {
+  .card--typo {
+    width: calc(100%  / 3 - 10px);
+
+  }
+}
+
+@media (min-width: 768px) {
+  .card--typo {
+    width: calc(100%  / 4 - 10px);
+  }
+}
+
+@media (min-width: 1024px) {
+  .card--typo {
+    width: calc(100%  / 5 - 10px);
+  }
+}
+
+@media (min-width: 1280px) {
+  .card--typo {
+    width: calc(100% / 6 - 10px);
+  }
+}
+
+@media (min-width: 1440px) {
+  .card--typo {
+    width: calc(100% / 7 - 10px);
+  }
+}
+
+@media (min-width: 1600px) {
+  .card--typo {
+    width: calc(100% / 8 - 10px);
+  }
+}
+
+
 </style>
