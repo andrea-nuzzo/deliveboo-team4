f12a1acf3871d19d3658fbbf106abb35d699029e
Sergjio97 <89703124+Sergjio97@users.noreply.github.com>
2022-03-16 16:35:26 +0100

    router link single restaurant

--- resources/js/guest/components/commons/CardUser.vue
+++ resources/js/guest/components/commons/CardUser.vue
@@ -2,12 +2,14 @@
   <div>
     <div class="container-fluid">
       <div class="row">
-        <div v-for="user in usersList" :key="user.id" class="col-xs-12 col-md-6 col-lg-3 cardUser p-3">
+        <div v-for="user in usersList" :key="user.id" class="col-xs-12 col-md-6 col-lg-3 cardUser p-3">   
             <div class="image">
-              <img :src="`/storage/${user.image}`" alt="Users-Image"/>
-              <div class="RestaurantName px-2 py-1">
-                {{user.name}}
-              </div>
+              <router-link :to="{ name: 'single-restaurant', params: {slug: user.slug}}"> 
+                <img :src="`/storage/${user.image}`" alt="Users-Image"/>
+                <div class="RestaurantName px-2 py-1">
+                  {{user.name}}
+                </div>
+              </router-link>
             </div>
         </div>
       </div>
