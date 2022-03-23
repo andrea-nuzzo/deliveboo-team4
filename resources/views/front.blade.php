adf876a1d8014aaa62820d17c6a47b26bbca4c95
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-03-23 09:50:55 +0100

    pyment

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -7,6 +7,16 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{asset("css/front.css")}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset("css/front.css")}}">
+</head>
+<body>
+    <div id="app"></div>
+
+    <script src="{{asset("js/front.js")}}"></script>
+</body>
+</html>
+
     <title>DeliveBoo</title>
 </head>
 <body>
