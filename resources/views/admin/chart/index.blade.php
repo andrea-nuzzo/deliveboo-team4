331c592f6debad55771654ada21cbdacc83862c7
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-03-27 22:51:29 +0200

    chart

--- resources/views/admin/chart/index.blade.php
+++ resources/views/admin/chart/index.blade.php
@@ -2,9 +2,47 @@
 
 @section('content')
     <div class="container">
-        {{$quantity}}
-
-        <h1>{{$name}}</h1>
     </div>
 
+<canvas id="myChart" width="400" height="400"></canvas>
+
+<script>
+
+    const ctx = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                
+                data:  {!! json_encode($prova) !!} ,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    </script>
 @endsection;
